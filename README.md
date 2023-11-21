000f4542785c601c2585dc62ed6ff7d2bcbaaf44
Tamara <29234416+tam92@users.noreply.github.com>
2023-11-21 13:21:11 +0100

    Added main menu open/close functionality on click. Added submenus show/hide functionality on mouseover.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This is an exercise from "The Odin Project".
 Link: https://www.theodinproject.com/lessons/node-path-javascript-dynamic-user-interface-interactions#mobile-menus
+
+
+# Credits
+- Menu icon: https://icons.getbootstrap.com/icons/three-dots-vertical/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-import "./style.css";
+import './style.css'
+
+const btnMenu = document.getElementById('btn-menu')
+const collapsableMenu = document.getElementById("collapsable-menu");
+let clickFlag = true;
+
+const submenu1 = document.getElementById("submenu-1");
+const ul1 = document.getElementById('ul-1');
+const submenu2 = document.getElementById("submenu-2");
+const ul2 = document.getElementById('ul-2');
+const submenu3 = document.getElementById("submenu-3");
+const ul3 = document.getElementById('ul-3');
+
+submenuEventListeners(submenu1, ul1);
+submenuEventListeners(submenu2, ul2);
+submenuEventListeners(submenu3, ul3);
+
+// -----------------------------------------------------------------------------
+// Shows/hides the main menu (click button)
+// -----------------------------------------------------------------------------
+btnMenu.addEventListener('click', () => {
+
+    if (clickFlag) {
+        collapsableMenu.removeAttribute("hidden");
+        clickFlag = false;
+    } else {
+        collapsableMenu.setAttribute("hidden", true);
+        clickFlag = true;
+    }
+})
+
+// -----------------------------------------------------------------------------
+// Shows/hides the submenus on mouse hover
+// -----------------------------------------------------------------------------
+function submenuEventListeners(submenuElement, ulElement) {
+    submenuElement.addEventListener('mouseover', () => {
+        ulElement.removeAttribute('hidden');
+    })
+    submenuElement.addEventListener('mouseout', () => {
+        ulElement.setAttribute('hidden', true);
+    })
+}
